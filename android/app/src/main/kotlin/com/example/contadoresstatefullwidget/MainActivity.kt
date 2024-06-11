2c6dc9d014f7dd9b9150f7c7dc7d565af88f8761
Klaux2020 <pervaldez.netscape.net>
2024-06-10 19:11:06 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/contadoresstatefullwidget/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.contadoresstatefullwidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:contadoresstatefullwidget/pages/dice_page.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: DicePage(),
+      debugShowCheckedModeBanner: false,
+  ),
+  );
+}
